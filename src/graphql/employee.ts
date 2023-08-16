e5f5ebcfcd8a20ebd499ed816f71987d5c2f490a
eunchae123 <eunchae123@jnfirst.co.kr>
2023-08-16 11:42:23 +0900

    ADD

--- src/graphql/employee.ts
+++ src/graphql/employee.ts
@@ -1,16 +1,23 @@
 import Upload from "graphql-upload/Upload.mjs";
 import { departments, employees } from "../data/data.js";
-import { IEmployee, IEmployeeInput, IEmployeeModInput, IFile } from "../data/types.js";
+import {
+  IEmployee,
+  IEmployeeInput,
+  IEmployeeModInput,
+  IFile,
+} from "../data/types.js";
 import fs from "fs";
 import { GraphQLError } from "graphql";
 
-const uploadFolder = "/Users/user/project/back/public/upload/";
+const uploadFolder = "/work/groupware-mock-server/public/upload/";
 
 export const getEmployee = (id: string): IEmployee | undefined => {
   const employee = employees.find((e) => e.employeeId === id);
   if (!employee) return undefined;
 
-  const department = departments.find((d) => d.departmentId === employee.departmentId);
+  const department = departments.find(
+    (d) => d.departmentId === employee.departmentId
+  );
   return { ...employee, department, passwd: "" };
 };
 
@@ -22,7 +29,10 @@ export const addEmployee = (input: IEmployeeInput): IEmployee | undefined => {
   return getEmployee(input.employeeId);
 };
 
-export const modEmployee = (id: string, input: IEmployeeModInput): IEmployee | undefined => {
+export const modEmployee = (
+  id: string,
+  input: IEmployeeModInput
+): IEmployee | undefined => {
   const employee = employees.find((e) => e.employeeId === id);
   if (!employee) return undefined;
 
@@ -35,15 +45,22 @@ export const modEmployee = (id: string, input: IEmployeeModInput): IEmployee | u
   employee.position = input.position;
   employee.startDate = input.startDate;
 
-  employee.department = departments.find((d) => d.departmentId === employee.departmentId);
+  employee.department = departments.find(
+    (d) => d.departmentId === employee.departmentId
+  );
   return employee;
 };
 
 export const saveFile = (file: Upload, userId: string): IFile => {
   const { filename, mimetype, encoding } = file.file!;
   const rStream = file.file!.createReadStream();
-  const newFileName = filename.replace(/[\w\d_-]+(\.[\w\d_-]+)$/i, userId + "$1");
-  const wStream = fs.createWriteStream(uploadFolder + newFileName, { flags: "w" });
+  const newFileName = filename.replace(
+    /[\w\d_-]+(\.[\w\d_-]+)$/i,
+    userId + "$1"
+  );
+  const wStream = fs.createWriteStream(uploadFolder + newFileName, {
+    flags: "w",
+  });
   rStream.pipe(wStream);
   return { filename: newFileName, mimetype, encoding };
 };
